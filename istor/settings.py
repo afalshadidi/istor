419deac46fafc47ace448c7a4d4da2ee292ccd47
afalshadidi <afalshadidi@gmail.com>
2025-07-26 12:22:53 +0300

    ✅ إعداد إرسال الإيميلات باستخدام Gmail عبر App Password

--- istor/settings.py
+++ istor/settings.py
@@ -121,3 +121,13 @@ CLOUDINARY_SECURE = True
 
 # النوع الافتراضي للحقول
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# إعدادات البريد الإلكتروني عبر Gmail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'afalshadidi@gmail.com'
+EMAIL_HOST_PASSWORD = 'fhteezqgjeukzmir'  # ✅ كلمة المرور بدون مسافات
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
